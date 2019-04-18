a2b53fde2b0d119b7d9534c51c36cb8ee48ecac2
xeoncross <david@xeoncross.com>
2019-04-18 10:07:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## Android Media Backups
+
+Sometimes there are connection problems with getting android file transfer to work (i.e. Google Pixel).
+
+1. Enable USB debugging on the device
+1. Download [Android Debug Bridge (adb)](https://developer.android.com/studio/command-line/adb)
+3. Install [Golang (Go)](http://golang.org)
+
+
+### Run
+
+Using the command line, you can check out this project, and run it using Go:
+
+    $ git clone git@github.com:Xeoncross/android_media_backup.git
+    $ cd android_media_backup
+    $ go run android_media_backup.go
+
+
+### More about adb
+
+Android Debug Bridge (adb) can be used directly from the terminal. You can use it to access the files manually for coping by starting a new shell instance to browse the device folders:
+
+    adb shell
+
+or run a command remotely
+
+    adb shell ls /sdcard/DCIM
+
+Then you can use `adb pull ...` to download the files locally.
+
+    adb pull -a /sdcard/DCIM/Camera/ ./
+    adb pull -a /sdcard/Snapseed ./
+    adb pull -a /sdcard/WhatsApp ./
---
+++ android_media_backup.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+//
+// Simple script to download&remove photos from an Android phone which
+// disconnects before you can get everything off (even using ADB).
+// Last used on a Google Pixel 2 which had a bad USB-C port.
+//
+
+var CameraDirectory = "Camera"
+
+func main() {
+
+	// Step 1: is the device connected?
+	b, err := exec.Command("adb", "devices").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out := string(bytes.TrimSpace(b))
+	fmt.Printf("%q\n", out)
+
+	parts := strings.Split(out, "\n")
+
+	if len(parts) == 1 {
+		log.Fatal("No devices found, connect phone")
+	}
+
+	if len(parts) != 2 {
+		log.Fatal("Too many devices listed, only connect one at a time")
+	}
+
+	parts = strings.Split(parts[1], "\t")
+	deviceID := parts[0]
+	fmt.Printf("Downloading from %s\n", deviceID)
+
+	// Step 2: what images have already been brought over?
+	if ok, _ := exists(CameraDirectory); !ok {
+		err = os.Mkdir(CameraDirectory, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	ComputerFiles, err := FilePathWalkDir(CameraDirectory)
+
+	if err != nil {
+		log.Fatal("Error looking at files", err)
+	}
+
+	// Step 3: What files are on the device?
+	b, err = exec.Command("adb", "shell", "ls", "/sdcard/DCIM/Camera").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("%q\n", b)
+
+	PhoneFiles := strings.Split(string(b), "\n")
+
+	// Step 4: Remove files on the phone we have saved locally
+	if len(PhoneFiles) > 0 {
+		for _, pf := range PhoneFiles {
+			var found bool
+			for _, cf := range ComputerFiles {
+				if cf == pf {
+					found = true
+					break
+				}
+			}
+
+			if found && len(pf) > 4 {
+				// fmt.Printf("REMOVE: %s\n", pf)
+				b, err = exec.Command("adb", "shell", "rm", filepath.Join("/sdcard/DCIM/Camera/", pf)).Output()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("REMOVE %s -> %q\n", pf, b)
+			}
+		}
+	}
+
+	// Step 4: Download new ones: adb pull -a /sdcard/DCIM/Camera/ ./
+	b, err = exec.Command("adb", "pull", "-a", "/sdcard/DCIM/Camera/", "./").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out = string(bytes.TrimSpace(b))
+	fmt.Println(out)
+	// Step 5: if at first you don't succeed, fail, fail again!
+
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+func FilePathWalkDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, filepath.Base(path))
+		}
+		return nil
+	})
+	return files, err
+}
+
+// 2019 http://davidpennington.me - Released under the MIT License
